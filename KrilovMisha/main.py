58a6fd12a4474c4ba836cae5cf000d88ee21bd20
fvend <fvendetta311@gmail.com>
2023-11-27 21:51:40 +0300

    T4 | fixed the parser

--- KrilovMisha/main.py
+++ KrilovMisha/main.py
@@ -5,16 +5,14 @@ import csv
 
 from bs4 import BeautifulSoup
 
-DEBBUGING = False
-
-fieldnames_analysis: list = ['Город', 'Код', 'Наименование', 'Срок', 'Стоимость услуги', 'Наименование лаборатории', 'Дата']
-fieldnames_centers: list = ['Город', 'Адреса офисов']
+fieldnames_analysis: list = ['Город', 'Код', 'Группа', 'Наименование', 'Срок', 'Стоимость услуги', 'Наименование лаборатории', 'Дата']
+fieldnames_centers: list = ['Город', 'Адрес', 'Контактные номера', 'Время работы', 'Станция метро']
 
 class Parser:
     __url: str = "https://www.cmd-online.ru"
-    __path_cities: str = "cities.json"
     __path_data_analysis = r".\data\analysis.csv"
     __path_data_centers = r".\data\centers.csv"
+    __city = 'sankt-peterburg'
 
     __headers: dict = {
         "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
@@ -26,51 +24,41 @@ class Parser:
         self.session.headers: dict = self.__headers
 
         self.data: dict = {}
-        self.cities: list = self.get_cities()
         self.date: datetime = datetime.datetime.now()
 
-    def get_cities(self) -> list:
-        with open(self.__path_cities) as file:
-            return json.load(file)
-
-    def get_soup(self, url: str) -> BeautifulSoup:
-        if DEBBUGING:
-            print(url)
-            
+    def get_soup(self, url: str) -> BeautifulSoup:            
         return BeautifulSoup(self.session.get(url).text, "lxml")
 
     def parse_page(self, url: str) -> list[dict]:
         page_data = []
 
         soup = self.get_soup(url)
+        for section in soup.find_all('section', class_='analyze-section'):
+            for analyze in section.find_all("article", class_="analyze-item"):
+                data = {}
 
-        for analyze in soup.find_all("article", class_="analyze-item"):
-            data = {}
+                data['Группа'] = section.find('h2', class_='analyze-section__title').text.strip()
 
-            data['Город'] = soup.find(
-                'li', class_='header__links-link header__links-link--city'
-            ).text.strip()
+                data["Наименование"] = analyze.find(
+                    "div", class_="analyze-item__title"
+                ).text.strip()
 
-            data["Наименование"] = analyze.find(
-                "div", class_="analyze-item__title"
-            ).text.strip()
+                data["Код"] = analyze.find_all(
+                    "dl", class_="analyze-item__spec"
+                )[0].text.replace("Код:", "").strip()
 
-            data["Код"] = analyze.find_all(
-                "dl", class_="analyze-item__spec"
-            )[0].text.replace("Код:", "").strip()
+                data["Срок"] = analyze.find_all(
+                    "dl", class_="analyze-item__spec"
+                )[1].text.replace("Срок:", "").strip()
 
-            data["Срок"] = analyze.find_all(
-                "dl", class_="analyze-item__spec"
-            )[1].text.replace("Срок:", "").strip()
+                data["Стоимость услуги"] = analyze.find(
+                    "div", class_="analyze-item__price"
+                ).text.replace("Цена:", "").strip()
 
-            data["Стоимость услуги"] = analyze.find(
-                "div", class_="analyze-item__price"
-            ).text.replace("Цена:", "").strip()
+                data['Наименование лаборатории'] = "CMD"
+                data['Дата'] = "{}.{}.{}".format(self.date.day, self.date.month, self.date.year)
 
-            data['Наименование лаборатории'] = "CMD"
-            data['Дата'] = "{}.{}.{}".format(self.date.day, self.date.month, self.date.year)
-
-            page_data.append(data)
+                page_data.append(data)
 
         return page_data
 
@@ -90,20 +78,28 @@ class Parser:
 
         return analysis
 
-    def parse_city_centers(self, city) -> list:
+    def parse_city_centers(self, city) -> dict:
         soup = self.get_soup(f"{self.__url}/patsientam/gde-sdat-analizy/{city}/")
 
+        return [self.parse_center(self.__url+i.find('a').get('href')) for i in soup.find_all('li', class_='office-list__item visible-office')]
+
+    def parse_center(self, url) -> dict:
+        soup = self.get_soup(url)
+        
         return {
-            "Город": soup.find('li', class_='header__links-link header__links-link--city').text.strip(),
-            "Адреса офисов": [i.text.strip() for i in soup.find_all('div', class_='office-item__address')]
+            'Город': soup.find('li', class_='header__links-link header__links-link--city').text.strip(),
+            'Адрес': soup.find('address', class_='med-office__address').text.strip(),
+            'Контактные номера': '; '.join([i.find('a').text.strip() for i in soup.find_all('ul', class_='phones')]),
+            'Время работы': '; '.join(f'{i.get("data-day")}: {i.text.strip()}' for i in soup.find('table', class_='custom-table').find('tbody').find('tr').find_all('td') if i.get('data-day')),
+            'Станция метро': ''
         }
 
     def parse(self) -> list:
-        for city in self.cities:
-            self.data[city] = {
-                'Анализы': self.parse_city_analysis(city),
-                'Адреса офисов': [self.parse_city_centers(city)]
-            }
+
+        self.data[self.__city] = {
+            'Анализы': self.parse_city_analysis(self.__city),
+            'Адреса офисов': self.parse_city_centers(self.__city)
+        }
 
         # Сохраняем данные адресов
         with open(self.__path_data_centers, 'w', encoding='cp1251', newline='') as file:
@@ -112,13 +108,12 @@ class Parser:
             for city in self.data:
                 writer.writerows(self.data[city]['Адреса офисов'])
 
-        # Сохраняем данные анализов
+        # # Сохраняем данные анализов
         with open(self.__path_data_analysis, 'w', encoding='cp1251', newline='') as file:
             writer = csv.DictWriter(file, fieldnames=fieldnames_analysis)
             writer.writeheader()
             for city in self.data:
                 writer.writerows(self.data[city]['Анализы'])
-
 
 Parser().parse()
 
